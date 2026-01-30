8c9faee212b4076573c34a5756682495df930d9f
Wilf-G <s2374867@students.ncl-coll.ac.uk>
2026-01-30 09:56:12 +0000

    base framework

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("checking da git")
-print("checking da git")
+import sqlite3
+
+def main_menu():
+    print("Food INC staff system")
+    try:
+        choice = int(input("1. View Current reciepts \n2. Monitor Stock"))
+        match choice:
+            case 1:
+                print("Current reciepts")
+            case 2:
+                print("Current stock")
+            case _:
+                print("Invalid choice")
+                main_menu()
+    except Exception as e:
+        print("error:", e)
+        main_menu()
