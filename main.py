8ca07f35f02d25c8a4de9ab8dcaf33c89b1d3b8d
hgachichinwokeukwu <166533998+hgachichinwokeukwu@users.noreply.github.com>
2026-02-05 19:36:33 +0000

    I have made some changes to the main.py file

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sqlite3
 def main_menu():
     print("Food INC staff system")
     try:
-        choice = int(input("1. View Current reciepts \n2. Monitor Stock \n >"))
+        choice = int(input("1. View Current receipts \n2. Monitor Stock \n >"))
         match choice:
             case 1:
-                print("Current reciepts")
+                print("Current receipts")
             case 2:
                 print("Current stock")
             case _:
