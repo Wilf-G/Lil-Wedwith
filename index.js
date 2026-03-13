8458713890464914f53201c99f1d15449c88002f
hgachichinwokeukwu <166533998+hgachichinwokeukwu@users.noreply.github.com>
2026-03-13 09:37:50 +0000

    Added a button to one of the elements

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 window.alert("Hello, World! This is a simple JavaScript alert.");
+
+const orderNowButton = document.getElementById("Order_Now1");
+orderNowButton.addEventListener("click", function() {
+    alert("Thank you for your order! Your delicious meal will be prepared shortly.");
+});
